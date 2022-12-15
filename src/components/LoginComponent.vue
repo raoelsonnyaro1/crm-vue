8236ed27c70e01aa3ba3785cd9318a053451fd36
raoelsonnyaro1 <raoelsonnyaro@gmail.com>
2022-12-15 11:46:20 +0300

    Update LoginComponent.vue

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -6,33 +6,27 @@
                 <v-flex xs12 sm8 md4>
                    <v-card class="elevation-12">
                       <v-toolbar dark color="primary">
-                         <v-toolbar-title>Login form</v-toolbar-title>
+                         <v-toolbar-title>Connectez-vous</v-toolbar-title>
                       </v-toolbar>
                       <v-card-text>
                          <v-form>
                             <v-text-field
-                               prepend-icon="person"
+                               prepend-icon="mdi-account"
                                name="login"
                                label="Login"
                                type="text"
                             >
-                            <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon></v-text-field>
+                            </v-text-field>
                             <v-text-field
                                id="password"
-                               prepend-icon="lock"
+                               prepend-icon="mdi-key"
                                name="password"
                                label="Password"
                                type="password"
                             ></v-text-field>
                          </v-form>
                       </v-card-text>
-                      <v-card-actions>
+                      <v-card-actions class="pa-6">
                          <v-spacer></v-spacer>
                          <v-btn color="primary" to="/">Login</v-btn>
                       </v-card-actions>
